8d9c5953f8f5947cc164a63af273ba975feb41ed
Him-anshu-Dev10 <himutech10@gmail.com>
2026-02-14 19:28:14 +0530

    deploy issue resolve

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,17 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          router: ["react-router-dom"],
+          ui: ["react-hot-toast", "quill"],
+          utils: ["axios", "moment", "marked"],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
 });
